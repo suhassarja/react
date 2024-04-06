761f3ca288c8187bb6dbab7bb129a89c2eca23ad
suhassarja <suhassarja@gmail.com>
2024-04-06 11:07:51 +0530

    Props and Dynamic values display

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -19,11 +20,35 @@ function Header() {
   );
 }
 
+function CoreConcept({image,title,description}){
+  return(
+    <li>
+      <img src={image} alt={title}></img>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>CORE CONCEPTS</h2>
+          <ul>
+            <CoreConcept 
+              title ={CORE_CONCEPTS[0].title}
+              description ={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
